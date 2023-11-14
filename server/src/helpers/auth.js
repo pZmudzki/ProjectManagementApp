7993567fb7614794db5461aeec2d3fd8f12cde69
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2023-11-15 00:08:23 +0100

    fakfajkfa

--- server/src/helpers/auth.js
+++ server/src/helpers/auth.js
@@ -22,23 +22,7 @@ const comparePassword = (password, hashedPassword) => {
   return bcrypt.compare(password, hashedPassword);
 };
 
-const authenticate = (req, res, next) => {
-  try {
-    const { token } = req.cookies;
-    if (!token) return res.status(401).json({ error: "Unauthorized" });
-
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified.user;
-
-    next();
-  } catch (err) {
-    console.error(err);
-    res.status(401).json({ error: "Unauthorized" });
-  }
-};
-
 module.exports = {
   hashPassword,
   comparePassword,
-  authenticate,
 };
